e5abb766d30d9b0c3e97f57cf710a7863a9d5816
Soo Hwangbo <soohwangbo@Soos-MacBook-Pro.local>
2022-07-09 16:51:02 -0700

    resonsive design fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,30 +2,35 @@ import React from "react";
 import {Navbar, Container, Nav} from "react-bootstrap";
 
 export default function NavbarTop() {
-  return (
-    <Container className="mx-3">
-      <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="/">
-            <p className="boldText pt-3">SOO</p>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto"></Nav>
-            <Nav>
-              <a className="navigation-link" href="#about">
-                About
-              </a>
-              <a className="navigation-link" href="#works">
-                Works
-              </a>
-              <a className="navigation-link" href="#contact">
-                Contact
-              </a>
-            </Nav>
-          </Navbar.Collapse>
+    return (
+        <Container className="mx-3">
+            <Navbar
+                fixed="top"
+                collapseOnSelect
+                expand="lg"
+                bg="dark"
+                variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">
+                        <p className="boldText pt-3">SOO</p>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto"></Nav>
+                        <Nav>
+                            <a className="navigation-link" href="#about">
+                                About
+                            </a>
+                            <a className="navigation-link" href="#works">
+                                Works
+                            </a>
+                            <a className="navigation-link" href="#contact">
+                                Contact
+                            </a>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </Container>
-      </Navbar>
-    </Container>
-  );
+    );
 }
